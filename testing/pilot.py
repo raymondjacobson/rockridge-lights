c31d52a96b1fc455fb8893b66d3ea77276810dff
Raymond Jacobson <raymondshujacobson8@gmail.com>
2021-12-04 16:13:29 -0800

    Get pilot working

--- testing/pilot.py
+++ testing/pilot.py
@@ -20,7 +20,8 @@ def on_progress(entries):
 	percent_complete = downloaded_bytes * 1. / total_bytes * 1.
 	print(percent_complete)
 
-song_url = 'https://www.youtube.com/watch?v=ftSUchAdVTE'
+#song_url = 'https://www.youtube.com/watch?v=ftSUchAdVTE'
+song_url = 'https://www.youtube.com/watch?v=aAkMkVFwAoo'
 parsed_song_url = parse.urlparse(song_url)
 song_key = parse.parse_qs(parsed_song_url.query)['v'][0]
 
@@ -40,7 +41,7 @@ if f"{song_key}.wav" not in existing_song_files:
 			'preferredquality': '192'
 		}],
 		'postprocessor_args': [
-			'-ar', '48000'
+			'-ar', '44100'
 		],
 		'prefer_ffmpeg': True,
 		'keepvideo': False,
@@ -57,21 +58,6 @@ if f"{song_key}.wav" not in existing_song_files:
 wf = wave.open(outfile, 'rb')
 print(wf.getsampwidth())
 
-"""
-import alsaaudio
-periodsize=wf.getframerate() // 8
-device = alsaaudio.PCM(
-	channels=wf.getnchannels(),
-	rate=wf.getframerate(),
-	format=alsaaudio.PCM_FORMAT_S16_LE,
-	periodsize=periodsize
-)
-
-data = wf.readframes(periodsize)
-while data:
-	device.write(data)
-	data = wf.readframes(periodsize)
-"""
 
 p = pyaudio.PyAudio()
 
@@ -80,47 +66,32 @@ for i in range(p.get_device_count()):
 	print((i,dev['name'],dev['maxInputChannels']))
 print(wf.getframerate())
 
-"""
+
 def reader(in_data, frame_count, time_info, status):
 	data = wf.readframes(frame_count)
 	human_data = np.frombuffer(data, dtype=np.int16)
 	peak = np.average(np.abs(human_data)) * 2
-	power = peak / 20000.
-	print(power)
-	pixel.fill((0, peak / 20000., 0))
+	power = peak / 40000.
+	r = min(power * 255, 255)
+	g = 0
+	b = 0
+	pixel.fill((g, r, b))
 	return (data, pyaudio.paContinue)
 
-"""
+
 stream = p.open(
 	format=p.get_format_from_width(wf.getsampwidth()),
 	channels=wf.getnchannels(),
 	rate=wf.getframerate(),
 	output=True,
-	output_device_index=0,
-	frames_per_buffer=48000,
-	#stream_callback = reader
+	frames_per_buffer=int(44100 / 60),
+	stream_callback = reader
 )
-"""
+
 while stream.is_active():
 	time.sleep(0.1)
 
 stream.stop_stream()
 stream.close()
 p.terminate()
-"""
-
-
-data = wf.readframes(CHUNK)
-while data != '':
-	stream.write(data)
-	data = wf.readframes(CHUNK)
-
-	#human_data = np.frombuffer(data, dtype=np.int16)
-	#peak = np.average(np.abs(human_data)) * 2
-	#power = peak / 20000.
-	#pixel.fill((0, peak / 20000., 0))
-
-stream.close()
-p.terminate()
-
 
